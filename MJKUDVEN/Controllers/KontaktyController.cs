06483a74d9b9067ae94ec1024ac4f330e46b98b6
mariusz.jurczenko@itformed.pl <mariusz.jurczenko@itformed.pl>
2015-09-26 14:31:01 +0200

    Models in an mvc application  Part 7

---
+++ MJKUDVEN/Controllers/KontaktyController.cs
@@ -0,0 +1,28 @@
+﻿using MJKUDVEN.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MJKUDVEN.Controllers
+{
+    public class KontaktyController : Controller
+    {
+        public ActionResult Szczegoly()
+        {
+            Kontakty kontakty = new Kontakty()
+            {
+                KontaktyId = 1,
+                Imie = "Marcin",
+                Nazwisko = "Jurczenko",
+                Miasto = "Katowice",
+                Ulica = "Adama 16/11",
+                Email = "marcin@wp.pl",
+                Telefon = "667-263-641"
+            };
+
+            return View(kontakty);
+        }
+    }
+}
---
+++ MJKUDVEN/Models/Kontakty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MJKUDVEN.Models
+{
+    public class Kontakty
+    {
+        public string Nazwisko;
+        public int KontaktyId { get; set; }
+        public string Nzawisko { get; set; }
+        public string Imie { get; set; }
+        public string Miasto { get; set; }
+        public string Ulica { get; set; }
+        public string Email { get; set; }
+        public string Telefon { get; set; }
+    }
+}
