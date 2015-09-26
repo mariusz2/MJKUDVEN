0ed0ee4a347a25bbb88353bf1e3094660115b707
mariusz.jurczenko@itformed.pl <mariusz.jurczenko@itformed.pl>
2015-09-26 18:11:24 +0200

    poprawki

--- MJKUDVEN/Models/Kontakty.cs
+++ MJKUDVEN/Models/Kontakty.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,7 @@ namespace MJKUDVEN.Models
     [Table("tblKontakt")]
     public class Kontakt
     {
+        [Key]
         public int KontaktyId { get; set; }
         public string Nazwisko { get; set; }
         public string Imie { get; set; }
